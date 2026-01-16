cb134ce044c6a2a0492d2cdf7a188fc69ad0c222
hd <shh@gmail.com>
2026-01-16 20:05:01 +0500

    done2

--- main.js
+++ main.js
@@ -1,60 +1,13 @@
-//Функции - это часть кода которая выполняет какие либо действия или вычисления, функцию можно использовать неограниченое количество раз
 
-//function declaretion
-
-// function info(one, two) {
-//     let result = (one + two)/2
-//     return result
-// }
-// console.log (info(100,200));
-
-// function expression
-
-// let info = function (one,two){
-//     let result = (one,two)/2
-//     return result
-// }
-// console.log (info(5,8));
-
-// function solve(min,max){
-//     let sum = 0
-//     for(let i = min; i<=max;i++){
-//       sum += i
-//     }
-//     return sum
-// }
-// console.log(solve(1,10))
-
-// Math.random()- отдаёт рандомное число от 0 до 0.99999999
-// Math.round() - округляет число как в математике
-// Math.ceil() - округляет до наибольшего
-// Math.floor() - округляет до наименьшего
-
-
-// function random(min,max){
-//     return Math.floor(Math.random()*(max+1-min)+min)
-// }
-
-// let a = +prompt("Ведите минимальный диапазон")
-// let b = +prompt("Ведите максимальный диапазон")
-
-
-// for(let i = 1;i<=5;i++){
-//     let one = random(a,b)
-//     let two = random(a,b)
-//     let primer = +prompt(one + "+" + two + "=")
-//     let word = primer == (one + two) ? "Молодец" : "Холодец"
-//     console.log(one + "+" + two + "=" + (one + two) + "Ваш ответ " + primer + "," + word)
-// }
-
-
-var z = prompt("Введите имя:")
-var year1 = +prompt("Введите год рождения:")
-var year2 = +prompt("Ведите нынешний год:")
-var age = (year2 - year1)
-console.log(z + ", " + "ваш возраст:" + age)
-
-
+ function i (l,k){
+  let age = l-k
+   return age
+ }
+  let t = prompt('Введите имя')
+  let year1= +prompt('Введите год рождения')
+  let year2= +prompt('Введите нынешний год')
+  console.log(t + ',' + ' Ваш возраст:' + i(year2,year1))
+  
 
 
 
